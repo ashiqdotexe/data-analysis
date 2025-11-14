27bded9f90e962e1491fbb970504f0966849aa31
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-11-14 19:21:08 +0600

    Simple SELECT statement

---
+++ SQL/2. Simple SELECT statements.sql
@@ -0,0 +1,11 @@
+# simple select statement
+select * 
+from parks_and_recreation.employee_demographics;
+
+# select a particular column
+select first_name
+from parks_and_recreation.employee_demographics;
+
+# select distinct value 
+select distinct gender
+from parks_and_recreation.employee_demographics;
