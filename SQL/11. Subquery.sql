7811220adf06f8020bc16b2b60690da31d93d230
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2025-11-16 13:48:37 +0600

    Subquery

---
+++ SQL/11. Subquery.sql
@@ -0,0 +1,23 @@
+# Subquery
+select *
+from employee_demographics 
+where employee_id in (select employee_id
+						from employee_salary
+                        where dept_id=1)
+;
+
+select first_name, salary,
+(select avg(salary)
+from employee_salary) as avg_salary
+from employee_salary;
+
+select avg(max_age) as "Average Age"
+from(select gender, 
+avg(age) as avg_age, 
+max(age) as max_age, 
+min(age) as min_age
+from employee_demographics
+group by gender
+) as Agr_age
+;
+
