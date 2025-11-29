dbf139107f015698f570ebf56adadbcc9a8ad9af
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-11-29 07:15:11 +0600

    Standardization

--- SQL/Projects/Project_1.sql
+++ SQL/Projects/Project_1.sql
@@ -56,3 +56,41 @@ from layoffs_staging;
 select *
 from layoffs_staging2
 where row_num>1;
+
+#Data standardization
+select layoffs_staging2.company, trim(company)
+from layoffs_staging2;
+
+update layoffs_staging2
+set company = trim(company)
+;
+
+
+select distinct country, trim(trailing '.' from country)
+from layoffs_staging2
+order by 1;
+
+update layoffs_staging2
+set country = trim(trailing '.' from country);
+
+#Changing date column
+
+select `date`
+from layoffs_staging2;
+
+update layoffs_staging2
+set `date` = str_to_date(`date`, '%m/%d/%Y');
+
+alter table layoffs_staging2
+modify column `date` date;
+
+
+
+
+
+
+
+
+
+
+
